17844986b5d824c4f4907630101cb444d6eb0a7e
shalomsantos <shalomsantos1234@gmail.com>
2026-09-21 11:14:47 -0300

    Alpha: 0.03

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ require __DIR__ . '/core-docs-index/bootstrap.php';
     <title>Módulos locais</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" rel="stylesheet">
+    <link rel="icon" href="/core-docs-index/favicon.ico" type="image/x-icon">
     <link href="/core-docs-index/style.css" rel="stylesheet">
 </head>
 
